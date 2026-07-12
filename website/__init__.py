02420bdfe60fc54c6fd6dda3025222ce45ef4fc9
game <shyamraj.p.r777@gmail.com>
2026-07-12 11:23:45 +0530

    Add sign-up/login with SQLAlchemy auth and HTML templates

--- website/__init__.py
+++ website/__init__.py
@@ -1,15 +1,28 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+
+db = SQLAlchemy()
+login_manager = LoginManager()
 
 def create_app():
     app = Flask(__name__)
 
     app.config['SECRET_KEY'] = 'hikkmnnaapp'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+
+    db.init_app(app)
+    login_manager.init_app(app)
+    login_manager.login_view = 'auth.login'
 
     from .views import views
     from .auth import auth
 
-    app.register_blueprint(views,url_prefix='/')
-    app.register_blueprint(auth,url_prefix='/')
+    app.register_blueprint(views, url_prefix='/')
+    app.register_blueprint(auth, url_prefix='/')
 
+    with app.app_context():
+        from . import models
+        db.create_all()
 
     return app
---
+++ website/models.py
@@ -0,0 +1,12 @@
+from . import db, login_manager
+from flask_login import UserMixin
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(150), unique=True, nullable=False)
+    username = db.Column(db.String(150), unique=True, nullable=False)
+    password = db.Column(db.String(150), nullable=False)
